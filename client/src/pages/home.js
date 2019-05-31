f994f80a053dd97b9a98a00ffaedd13e9a2e8e85
parrn1 <mrnickp@comcast.net>
2019-05-31 11:27:13 -0400

    adding bootstrap and color pallette

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -7,13 +7,16 @@ class Home extends Component
     postsList: []
   };
 
-  componentDidMount() {
+  componentDidMount()
+  {
     this.handleGetPosts();
   }
 
-  handleGetPosts = () => {
+  handleGetPosts = () =>
+  {
     getPosts()
-      .then(({ data: postsList }) => {
+      .then(({ data: postsList }) =>
+      {
         this.setState({ postsList });
       })
       .catch(err => console.log(err));
@@ -26,22 +29,32 @@ class Home extends Component
       <React.Fragment>
 
         <h1>overworkshop</h1>
-        
-        {
-          this.state.postsList.map(post => {
-                return (
-                  
-                  <div>
-                  <h2>{post.title}</h2>
 
-                  <p>{post.link}</p>
-                  <p>{post.desc}</p>
-                      
-                    </div>
-                  
+        <div className="container">
+
+          <div className="row">
+            {
+              this.state.postsList.map(post =>
+              {
+                return (
+
+
+                  <div className="col-12 col-md-6">
+
+                    <h2>{post.title}</h2>
+
+                    <p>{post.link}</p>
+                    <p>{post.desc}</p>
+
+                  </div>
+
+
                 )
-          })
-        }
+              })
+            }
+          </div>
+
+        </div>
 
       </React.Fragment>
     )
