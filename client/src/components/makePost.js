c937fdbe5f6e8bae4d97a8d09ebc78b884aa3022
parrn1 <mrnickp@comcast.net>
2019-06-05 13:15:35 -0400

    can now go to correct post pages

---
+++ client/src/components/makePost.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import { addPost } from '../utils/postapi';
+
+class MakePost extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        title: '',
+        desc: '',
+        link: ''
+      };
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      const { name, value } = event.target;
+      this.setState({[name]: value});
+    }
+  
+    handleSubmit(event) {
+      event.preventDefault();
+      console.log('A name was submitted: ' + JSON.stringify(this.state));
+      alert('A name was submitted: ' + JSON.stringify(this.state));
+
+//need to update this to work with images /4***************************************
+
+//*************************************************** */
+
+      const post = {
+        title: this.state.title,
+        link: this.state.link,
+        desc: this.state.desc
+      }
+      console.log(JSON.stringify(post)  + "  add post")
+
+      addPost(post)
+        .then(postResponse => {
+          console.log(postResponse.data);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+
+
+    }
+
+  render() {
+    return (
+      <React.Fragment>
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-row">
+            <div className="form-group col-md-6">
+              <label htmlFor="Title">Title</label>
+              <input
+              value={this.state.title} 
+              onChange={this.handleChange}
+                type="text"
+                className="form-control"
+                id="title"
+                placeholder="Title"
+                name="title"
+              />
+            </div>
+            <div className="form-group col-md-6">
+              <label htmlFor="UniqueId">Unique ID</label>
+              <input
+                type="text"
+                value={this.state.link} 
+              onChange={this.handleChange}
+                className="form-control"
+                id="link"
+                placeholder="ID"
+                name="link"
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <label htmlFor="Description">Description</label>
+            <textarea className="form-control" id="desc" rows="3" value={this.state.desc} name="desc" 
+              onChange={this.handleChange} />
+          </div>
+          <input id="submit" type="submit" value="submit" className="btn btn-success btn-lg"/>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MakePost;
