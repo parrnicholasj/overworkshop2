91db1cbdf440c2b6abec94404c78d32a14928415
Isabella Torres <izzy612@gmail.com>
2019-06-06 23:34:19 -0400

    some changes

--- models/post.js
+++ models/post.js
@@ -24,7 +24,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: true,
       defaultValue:
-        'https://i.etsystatic.com/13105519/r/il/347952/1187963224/il_794xN.1187963224_fkxr.jpg',
+        'https://scontent.fewr1-3.fna.fbcdn.net/v/t31.0-8/906416_10103157316469369_986850834_o.jpg?_nc_cat=0&oh=ff1c6ad4081e24bae8e9cd57ced0b182&oe=5B8306B7',
       validate: {
         isUrl: true
       }
