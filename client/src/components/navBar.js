02c6f376d3af50806ad2aa0cb4eea09ebdb14ae1
harsh <harshrpatel93@gmail.com>
2019-06-10 15:21:23 -0400

    trying comments

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -17,27 +17,12 @@ class NavBar extends Component
     return (
       <React.Fragment>
         
-<<<<<<< HEAD
-        
-        
-          <h1 className="display-4 text-light bg-transparent py-3 m-3
-           pl-4">Overworkshops
-           {!this.state.loggedIn && (
-            
-            <Login reverse={this.reverseLogIn} loggedIn={this.state.loggedIn} />
-           )}
-           
-            </h1>
-         
-       
-=======
         <div className="row justify-content-center">
         <a href="/">
           <h1 className="navbar display-4 text-light bg-dark py-3 m-3 pl-4">Overworkshop <Login /></h1>
           
           </a>
           </div>
->>>>>>> 6f04f642054acb20ca692d4613eed56dfb737186
        
 
 
--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,13 +1,15 @@
 import React from 'react';
-<<<<<<< HEAD
 import { Button } from 'react-bootstrap';
 import { loginUser } from '../utils/userApi'
 import { Link } from 'react-router-dom';
 
+
+const path = process.env.NODE_ENV === "production" ? "YOUR HEROKU LINK/auth/google" : "http://localhost:3001/auth/google"
 class Login extends React.Component {
 
   login() {
     loginUser().catch(err => console.log(err))
+    
   }
 
   render () {
@@ -19,21 +21,10 @@ class Login extends React.Component {
           </Button> */}
 
           <a
-            className="nav-link" href="http://localhost:3001/auth/google">
+            className="nav-link" href={path}>
             <button type="button" className="btn btn-gplus btn-danger"><i className="fab fa-google-plus-g pr-1">Google+</i></button>
           </a>
         </div>
-=======
-
-function LoginButton() {
-  return (
-    <div className="container">
-      <div className="row justify-content-end">
-      <a
-           className="nav-link" href="http://localhost:3001/auth/google">
-           <button type="button"  className="btn btn-gplus btn-danger"><i className="fab fa-google-plus-g pr-1">Google+ login</i></button>
-         </a>
->>>>>>> 6f04f642054acb20ca692d4613eed56dfb737186
       </div>
     );
   }
